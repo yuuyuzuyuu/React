edc5dce814cf0dd3f201dc0ce77fe4709078ba12
Yu Sato <ym.74.mn@gmail.com>
2022-01-17 22:25:08 +0900

    再開ボタン追加

--- src/index.js
+++ src/index.js
@@ -75,6 +75,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -92,6 +103,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <div>{moves}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
